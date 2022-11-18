f4fe180569de6562362f81bdef4dac1a3266c8fc
caemuller <caemuller1@gmail.com>
2022-11-18 13:16:19 -0300

    asd

--- Parallel-codes/prime-numbers.cpp
+++ Parallel-codes/prime-numbers.cpp
@@ -3,10 +3,12 @@
 #include <cstdlib>
 #include <omp.h>
 #include <time.h>
+#include <chrono>
+#include <getopt.h>
 
 #define MAX 10000000000000
 #define MIN 100000
-#define NN 100
+#define NN 1000
 
 using namespace std;
 
@@ -15,19 +17,29 @@ int main(int argc, char *argv[]) {
     srand(time(0));
 
     int prime_count = 0;
+    int m=1, nthreadsn=1;
 
-    #pragma omp parallel for num_threads(nthreads) reduction(+:prime_count)
-    for(int i=0;i<NN;i++){
-        int n = rand() % (MAX - MIN + 1) + MIN;
-        int sup = 1;
-        for(int j=2;j<n;j++){
-            if(n%j==0){
-                sup=0;
-                break;
+    for(int k=0;k<5;k++)
+    {
+        auto start = chrono::high_resolution_clock::now();
+        #pragma omp parallel for num_threads(nthreadsn) reduction(+:prime_count)
+        for(int i=0;i<NN*m;i++){
+            int n = rand() % (MAX - MIN + 1) + MIN;
+            int sup = 1;
+            for(int j=2;j<n;j++){
+                if(n%j==0){
+                    sup=0;
+                    break;
+                }
             }
+            if(sup)
+                prime_count++;
         }
-        if(sup)
-            prime_count++;
+        auto finish = chrono::high_resolution_clock::now();
+        m=m*10;
+        nthreadsn++;
+        auto time = chrono::duration<double>(finish - start).count();
+        //write time in csv
     }
 
     cout<<"There are: " << prime_count << " prime numbers in the " << NN << " Generated Numbers" << endl;
