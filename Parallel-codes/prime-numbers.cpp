6d44e947d981118ecae3baa200b50486499d5e25
caemuller <caemuller1@gmail.com>
2022-11-18 14:55:20 -0300

    asd

--- Parallel-codes/prime-numbers.cpp
+++ Parallel-codes/prime-numbers.cpp
@@ -5,6 +5,7 @@
 #include <time.h>
 #include <chrono>
 #include <getopt.h>
+#include <fstream>
 
 #define MAX 10000000000000
 #define MIN 100000
@@ -16,30 +17,41 @@ int main(int argc, char *argv[]) {
     int nthreads = atoi(argv[1]);
     srand(time(0));
 
+    fstream fout;
+    fout.open("pn-times.csv", ios::out | ios::app);
+    for(int i=1;i<nthreads+1;i++)
+        fout << i << ",";
+    fout << endl;
+
     int prime_count = 0;
     int m=1, nthreadsn=1;
 
     for(int k=0;k<5;k++)
     {
-        auto start = chrono::high_resolution_clock::now();
-        #pragma omp parallel for num_threads(nthreadsn) reduction(+:prime_count)
-        for(int i=0;i<NN*m;i++){
-            int n = rand() % (MAX - MIN + 1) + MIN;
-            int sup = 1;
-            for(int j=2;j<n;j++){
-                if(n%j==0){
-                    sup=0;
-                    break;
+        for(int l=1;l<nthreads+1;l++)
+        {
+            auto start = chrono::high_resolution_clock::now();
+            #pragma omp parallel for num_threads(l) reduction(+:prime_count)
+            for(int i=0;i<NN*m;i++){
+                int n = rand() % (MAX - MIN + 1) + MIN;
+                int sup = 1;
+                for(int j=2;j<n;j++){
+                    if(n%j==0){
+                        sup=0;
+                        break;
+                    }
                 }
+                if(sup)
+                    prime_count++;
             }
-            if(sup)
-                prime_count++;
+            auto finish = chrono::high_resolution_clock::now();
+            auto time = chrono::duration<double>(finish - start).count();
+            nthreadsn++;
+            fout << time << ",";
+            //write time in csv
         }
-        auto finish = chrono::high_resolution_clock::now();
+        fout << endl;
         m=m*10;
-        nthreadsn++;
-        auto time = chrono::duration<double>(finish - start).count();
-        //write time in csv
     }
 
     cout<<"There are: " << prime_count << " prime numbers in the " << NN << " Generated Numbers" << endl;
